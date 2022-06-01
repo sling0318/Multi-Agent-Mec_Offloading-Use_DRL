8218644837c5eee3fda84925648069bb5821d962
sling <59446568+sling0318@users.noreply.github.com>
2022-06-01 17:41:37 +0800

    Update env.py

--- env.py
+++ env.py
@@ -19,8 +19,8 @@ class ENV():
         self.actions = np.hstack((q, p))
         self.n_actions = len(self.actions)
         self.n_features = 2 + MECs * 3
-        self.beta_local, self.cycle_perbyte, self.energy_per_l = 0.4, 1, 6
-        self.beta_re, self.energy_per_r, self.discount = 0.2, 0.3, 0.01
+        self.beta_local, self.cycle_perbyte, self.beta_tr = 0.4, 1, 10,   # 本地能耗， CPU周期， 传输能耗
+        self.beta_re, self.energy_per_r, self.discount = 0.1, 0.3, 0.01
         self.local_core_max, self.local_core_min = 200, 50
         self.server_core_max, self.server_core_min = 400, 150
         self.uplink_max, self.uplink_min = 350, 100
@@ -40,7 +40,7 @@ class ENV():
                 up = np.random.randint(150, 200)
                 down = np.random.randint(300, 500)
                 if new_cap:
-                    cap = np.random.randint(200, 300)
+                    cap = np.random.randint(300, 400)   # MEC计算能力
                     servers_cap.append(cap)
                 uplink.append(up)
                 downlink.append(down)
@@ -52,7 +52,7 @@ class ENV():
 
     def choose_action(self, prob):
         """
-        根据概念选择动作
+        根据概率选择动作
         :param env:
         :param prob:
         :return: [[target_server, percentage]]
@@ -67,22 +67,23 @@ class ENV():
             actions.append(action)
         return actions
 
-    def step(self, observation, actions_prob, time1):
-        actions = self.choose_action(actions_prob)
+    def step(self, observation, actions_prob, time1, is_prob=True, is_compared=True):
+        if is_prob:
+            actions = self.choose_action(actions_prob)
+        else: actions = actions_prob
         new_cap = False
         obs_ = []
-        rew = []
-        local = []
-        ran = []
-        mec = []
+        rew, local, ran, mec = [], [], [], []
+        dpg_times, local_times, ran_times, mec_times = [], [], [], []
+        dpg_energys, local_energys, ran_energys, mec_energys = [], [], [], []
         for i in range(self.UEs):
             if i == self.UEs - 1: new_cap = True
             # 提取信息
             workload, local_comp, servers_cap, uplink, downlink = \
                 observation[i][0], observation[i][1], observation[i][2:2+self.MECs], observation[i][2+self.MECs:2+self.MECs*2], observation[i][2+self.MECs*2:2+self.MECs*3]
             wait_local, wait_server = np.random.randint(0, 2), np.random.randint(0, 3)
-            prob = actions_prob[i]
             action = actions[i]
+            # print("action:", action)
             target_server, percen = int(action[0]), action[1]
 
             # 计算奖励
@@ -95,23 +96,31 @@ class ENV():
             remote_time = workload * self.cycle_perbyte * percen / (servers_cap[target_server]) + wait_server + \
                           workload * percen / (uplink[target_server]) + self.discount * workload / (downlink[target_server])
 
-            remote_energy = self.beta_re * workload * percen
+            remote_energy = self.beta_re * workload * percen + self.beta_tr * workload * percen/(uplink[target_server]) + self.beta_tr * self.discount * workload / (downlink[target_server])
 
-            time_cost = local_time + remote_time
+            time_cost = max(local_time, remote_time)
 
             energy_cost = local_energy + remote_energy
 
             total_cost = 0.6 * time_cost + 0.4 * energy_cost
+            total_cost2 = energy_cost
 
             reward = -total_cost
 
             # 全本地
-            local_only = workload * self.cycle_perbyte / (local_comp) + wait_local + self.beta_local * workload
+            local_only_time = workload * self.cycle_perbyte /(local_comp)
+
+            local_only_energy = self.beta_local * workload
+
+            local_only = 0.6 * local_only_time + 0.4 * local_only_energy
 
             # 全边缘
-            remote_only = workload * self.cycle_perbyte / (servers_cap[target_server]) + wait_server + \
-                          workload / (uplink[target_server]) + self.discount * workload / (downlink[target_server]) + \
-                          self.beta_re * workload
+            remote_only_time = workload * self.cycle_perbyte / (servers_cap[target_server]) + wait_server + \
+                          workload / (uplink[target_server]) + self.discount * workload / (downlink[target_server])
+
+            remote_only_energy = self.beta_re * workload + self.beta_tr * workload / (uplink[target_server]) + self.beta_tr * self.discount * workload / (downlink[target_server])
+
+            remote_only = 0.6 * remote_only_time + 0.4 * remote_only_energy
 
             # 随机卸载
             percen_ran = np.random.uniform()
@@ -124,13 +133,14 @@ class ENV():
                           workload * percen_ran / (uplink[mec_ran]) + self.discount * workload / (
                           downlink[mec_ran])
 
-            random_remote_energy = self.beta_re * workload * percen_ran
+            random_remote_energy = self.beta_re * workload * percen_ran + self.beta_tr * workload * percen_ran/(uplink[target_server]) + self.beta_tr * self.discount * workload / (downlink[target_server])
 
-            random_time_cost = random_local_time + random_remote_time
+            random_time_cost = max(random_local_time, random_remote_time)
 
             random_energy_cost = random_local_energy + random_remote_energy
 
             random_total_cost = 0.6 * random_time_cost + 0.4 * random_energy_cost
+            random_total_cost2 = random_energy_cost
 
 
             # 得到下一个observation
@@ -166,5 +176,17 @@ class ENV():
             local.append(local_only)
             ran.append(random_total_cost)
             mec.append(remote_only)
-        return obs_, rew, local, mec, ran
+            dpg_times.append(time_cost)
+            local_times.append(local_only_time)
+            ran_times.append(random_time_cost)
+            mec_times.append(remote_only_time)
+            dpg_energys.append(energy_cost)
+            local_energys.append(local_only_energy)
+            ran_energys.append(random_energy_cost)
+            mec_energys.append(remote_only_energy)
+
+        if is_compared:
+            return obs_, rew, local, mec, ran, dpg_times, local_times, mec_times, ran_times, dpg_energys, local_energys, mec_energys, ran_energys
+        else:
+            return obs_, rew
 
